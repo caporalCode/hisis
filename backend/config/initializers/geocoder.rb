d333e420bdc16f476deb5dad7addc76752fbe27f
caporalCode <idealprogrammeur@gmail.com>
2024-11-05 21:17:16 +0100

    config geocoder

---
+++ backend/config/initializers/geocoder.rb
@@ -0,0 +1,8 @@
+Geocoder.configure(
+  # Geocoding options
+  # lookup: :nominatim,
+  # api_key: ENV['GOOGLE_MAPS_API_KEY'], # Make sure this is set correctly
+  timeout: 5, # seconds
+  units: :km, # defaults to :mi
+  http_headers: { 'User-Agent' => 'hisis' }
+)
